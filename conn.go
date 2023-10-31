e63238bb0dcfca3e5f8586359c3f62633486102b
Karancse <83109484+Karancse@users.noreply.github.com>
2023-10-31 18:10:55 +0530

    Update conn.go with testing code

--- conn.go
+++ conn.go
@@ -47,8 +47,7 @@ func (c *Conn) Close() error {
 }
 
 //Query method executes the statement with out prepare if no args provided, and a driver.ErrSkip otherwise (handled by sql.go to execute usual preparedStmt)
-func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {
-	if len(args) > 0 {
+func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {	if len(args) > 0 {
 		// Not implemented for queries with parameters
 		return nil, driver.ErrSkip
 	}
@@ -60,42 +59,57 @@ func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {
 	}
 	h := api.SQLHSTMT(out)
 
-	//	Edited Code  
-	
-	// number of rows to fetch using SQLFetchScroll
-	nR := 1
-	
-	/*  Set the number of rows fetched during SQLFetchScroll */
-	ret1 := api.SQLSetStmtAttr(h,api.SQL_ATTR_ROW_ARRAY_SIZE,
-		(api.SQLPOINTER)(nR),api.SQL_IS_INTEGER)
-	if IsError(ret1) {
-		return nil, NewError("SQLSetStmtAttr", h)
-	}
+	//	Edited Code Part 1  
 
-	/*  Set the cursor type to Dynamic cursor */
-	ret1 = api.SQLSetStmtAttr(h,api.SQL_ATTR_CURSOR_TYPE,
-		//(api.SQLPOINTER)(api.SQL_CURSOR_DYNAMIC),api.SQL_IS_INTEGER)
-		api.SQL_CURSOR_DYNAMIC,api.SQL_IS_INTEGER)
-	if IsError(ret1) {
-		return nil, NewError("SQLSetStmtAttr", h)
-	}
-
-	/*  Number of rows fetched during SQLFetchScroll will be stored in numrowsfetchedptr */
-	var numrowsfetchedptr uint64
-	ret1 = api.SQLSetStmtAttr(h,api.SQL_ATTR_ROWS_FETCHED_PTR,
-		(api.SQLPOINTER)(unsafe.Pointer(&numrowsfetchedptr)),api.SQL_IS_INTEGER)
-		//(api.SQLPOINTER)(numrowsfetchedptr),api.SQL_IS_INTEGER)
-	if IsError(ret1) {
-		return nil, NewError("SQLSetStmtAttr", h)
-	}
-
-	/* Setting b1 as the row status pointer */
+	nR := 1  // nR = maximum number of rows to fetch using SQLFetchScroll
+	var numrowsfetched uint32  
 	var b1 = make([]byte,nR)
-	ret1 = api.SQLSetStmtAttr(h,api.SQL_ATTR_ROW_STATUS_PTR,
-		(api.SQLPOINTER)(unsafe.Pointer(&b1[0])),api.SQL_IS_INTEGER)
-	if IsError(ret1) {
-		return nil, NewError("SQLSetStmtAttr", h)
+	if (true) {
+
+		/*  Sets the maximum number of rows fetched during SQLFetchScroll.
+			It is working. But (*rows r).Scan() is not working when nR > 1 */
+		ret = api.SQLSetStmtAttr(h,api.SQL_ATTR_ROW_ARRAY_SIZE,
+			(api.SQLPOINTER)(nR),api.SQL_IS_INTEGER)
+		if IsError(ret) {
+			return nil, NewError("SQLSetStmtAttr", h)
+		}
+
+		/* Sets the cursor type to dynamic.
+			But it is not working */
+		ret = api.SQLSetStmtAttr(h,api.SQL_ATTR_CURSOR_TYPE,
+			api.SQL_CURSOR_DYNAMIC,0)
+		if IsError(ret) {
+			return nil, NewError("SQLSetStmtAttr", h)
+		}
+		
+		/* number of rows fetched during SQLFetchScroll will be stored at numrowsfetched. */
+		//var numrowsfetched uint32
+		ret = api.SQLSetStmtAttr(h,api.SQL_ATTR_ROWS_FETCHED_PTR,
+			(api.SQLPOINTER)(unsafe.Pointer(&numrowsfetched)),api.SQL_IS_INTEGER)
+		if IsError(ret) {
+			return nil, NewError("SQLSetStmtAttr", h)
+		}
+
+		/* the status of the rows fetched during SQLFetchScroll are stored at b1.  */
+		//var b1 = make([]byte,nR)
+		ret = api.SQLSetStmtAttr(h,api.SQL_ATTR_ROW_STATUS_PTR,
+			(api.SQLPOINTER)(unsafe.Pointer(&b1[0])),api.SQL_IS_INTEGER)
+		if IsError(ret) {
+			return nil, NewError("SQLSetStmtAttr", h)
+		}
+
+		/* SQLGetStmtAttr stores output in buf2. */ 
+		buf2 := make([]byte,40)
+		fmt.Println("buf2 before get = ",buf2)
+		ret = api.SQLGetStmtAttr(h,api.SQL_ATTR_ROWS_FETCHED_PTR,
+			buf2,40)
+			//(api.SQLPOINTER)(unsafe.Pointer(&buf2[0])),40)
+		fmt.Println("buf2 after  get = ",buf2)
+		if IsError(ret) {
+			return nil, NewError("SQLGetStmtAttr", h)
+		}
 	}
+	// Edited Code Part 1 Ends
 	
 	drv.Stats.updateHandleCount(api.SQL_HANDLE_STMT, 1)
 	b := api.StringToUTF16(query)
@@ -118,11 +132,48 @@ func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Edited Code Part 2
+
 	if (true) {
-		ret1 = api.SQLSetPos(h, 1, api.SQL_UPDATE, api.SQL_LOCK_NO_CHANGE);
-		if IsError(ret1) {
+		ret = api.SQLFetch(h)
+		if ret == api.SQL_NO_DATA {
+			return nil, NewError("SQLFetch", h)
+		}
+		if IsError(ret) {
+			return nil, NewError("SQLFetch", h)
+		}
+	} else {
+		/* SQLFetchScroll works only for nR or SQL_ATTR_ROW_ARRAY_SIZE = 1.
+			Otherwise not working at (*rows r).Scan() */
+		ret = api.SQLFetchScroll(h,api.SQL_FETCH_FIRST,0)
+		if ret == api.SQL_NO_DATA {
+			return nil, NewError("SQLFetchScroll", h)
+		}
+		if IsError(ret) {
+			return nil, NewError("SQLFetchScroll", h)
+		}
+	}
+
+	fmt.Println("(api.SQLPOINTER)(unsafe.Pointer(&numrowsfetched)) = ",(api.SQLPOINTER)(unsafe.Pointer(&numrowsfetched)))
+	fmt.Println("numrowsfetched = ",numrowsfetched)
+	fmt.Println("b1 = ",b1)
+	v, err := os.Cols[0].Value(os.h, 0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("os.Cols[0].Value(os.h, 0) = ",v)
+
+	if (false) {
+		/* SQLSetPos is updating for row 1. 
+			But it is not working because cursor type is not set to dynamic */
+		ret = api.SQLSetPos(h, 1, api.SQL_UPDATE, api.SQL_LOCK_NO_CHANGE);
+		if IsError(ret) {
 			return nil, NewError("SQLSetPos", h)
 		}
 	}
+
+	// Edited Code Part 2 Ends
+
 	return &Rows{os: os}, nil
 }
